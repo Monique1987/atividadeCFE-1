ab071ad732e54f3bfc440af408aa840d01421146
Monique Ferreira <monique.mferreira@hotmail.com>
2023-02-12 14:10:12 -0300

    atividade

---
+++ script.js
@@ -0,0 +1,31 @@
+/*Efeito de botão voltar ao topo*/
+
+function topo(){
+    window.scrollTo(
+        {
+            top: 0;
+            left: 0;
+            behavior: "smooth"
+
+        }
+    )
+}
+
+/*Validação de login*/
+
+function login(){
+    var logado = 0;
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+
+if(usuario == 'admin' && senha == '123456'){
+    window.location = "index.html";
+    logado = 1;   
+}
+
+if (logado == 0){
+    alert("Acesso negado. Dados Incorretos!")
+}
+}
+    
